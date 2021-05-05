519e577751c8b4505e6a2a642407127e6edb862f
thajones <jones.thayil@outlook.com>
2021-05-05 18:32:17 +0530

    binary api input

--- FTSol/ftcrypt/urls.py
+++ FTSol/ftcrypt/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from ftcrypt import views
 from ftcrypt.views import ECRTFILE
 from django.conf.urls import url
 
 urlpatterns = [
-    url('', ECRTFILE.as_view(), name='file-upload'),
+    url('asdas/', ECRTFILE.as_view(), name='file-upload'),
+    path('', views.ecrtfil, name='ecrtfil'),
 ]
--- FTSol/ftcrypt/views.py
+++ FTSol/ftcrypt/views.py
@@ -1,12 +1,25 @@
+from datetime import datetime
+
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+# from django.middleware.csrf import get_token
 
 from ftspl.settings import mediapath, gpg, C_Email, ftp, S_Key, S_Key, S_Passkey
 from .serializers import FTCRYPTSerializer
 import os
 import gnupg
+
+@csrf_exempt
+def ecrtfil(request):
+    result = 'Post Binary Data...'
+    if request.method == 'POST':
+        f = str(request.body).split("'")[1]
+        result = ftecrt(f)
+    return HttpResponse(result)
 
 class ECRTFILE(APIView):
     parser_classes = (MultiPartParser, FormParser)
@@ -30,6 +43,17 @@ def ftecrt(filename):
     f = ''
     with open(mediapath + filename, 'r') as file:
         f = file.read()
+    status = gpg.encrypt(f, recipients=[C_Email], sign=S_Key, passphrase=S_Passkey,output=resultfile)
+    print("ok: ", status.ok)
+    print("status: ", status.status)
+    print("stderr: ", status.stderr)
+    # ftp.storbinary('STOR ' + resultfile, open(resultfile, 'rb'))
+    # ftp.quit()
+    return str(resultfile)
+
+def ftecrt(f):
+    formatedDate = datetime.now().strftime("%Y%m%d%H%M%S%f")
+    resultfile = mediapath + "\\encrypted\\" + str(formatedDate) + ".txt"
     status = gpg.encrypt(f, recipients=[C_Email], sign=S_Key, passphrase=S_Passkey,output=resultfile)
     print("ok: ", status.ok)
     print("status: ", status.status)
